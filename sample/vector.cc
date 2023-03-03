f804a26b8686f8e91938278a76e2f35c967bad58
Tateishi Tadatoshi <ishio39@gmail.com>
2023-03-04 02:27:43 +0900

    const 修飾を追加

--- sample/vector.cc
+++ sample/vector.cc
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-size_t read_number(string prompt) {
+size_t read_number(const string prompt) {
     cout << prompt;
     size_t ret;
     cin >> ret;
@@ -13,14 +13,14 @@ size_t read_number(string prompt) {
 }
 
 template<typename INT>
-void populate_vector(vector<INT>& v, size_t n) {
+void populate_vector(vector<INT>& v, const size_t n) {
     for (auto i = 0; i < n; ++i) {
         v.emplace_back(i);
     }
 }
 
 template<typename INT>
-vector<INT>&& populate_vector_ref(size_t n) {
+vector<INT>&& populate_vector_ref(const size_t n) {
     auto v = new vector<INT>;
 
     for (auto i = 0; i < n; ++i) {
@@ -31,17 +31,22 @@ vector<INT>&& populate_vector_ref(size_t n) {
 }
 
 template<typename INT>
-void print_vector(vector<INT>& v) {
+void print_vector(const vector<INT>& v) {
     for (auto item: v) {
         cout << item << ", ";
     }
     cout << endl;
 }
 
+void populate_and_print(const size_t n) {
+    print_vector(populate_vector_ref<uint32_t>(n));
+}
+
 int main() {
     auto n = read_number("数字を入力してください > ");
     auto v = populate_vector_ref<uint32_t>(n);
     print_vector(v);
+//    populate_and_print(n);
 
     return 0;
 }
