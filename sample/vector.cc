0df96cd7365ab2a872ee4295629288b20eb7d7d9
Tateishi Tadatoshi <ishio39@gmail.com>
2023-03-04 02:20:52 +0900

    ベクタサイズを入力するように変更・ベクタを作成する関数作成

--- sample/vector.cc
+++ sample/vector.cc
@@ -1,14 +1,33 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <cstdint>
 
 using namespace std;
 
+size_t read_number(string prompt) {
+    cout << prompt;
+    size_t ret;
+    cin >> ret;
+    return ret;
+}
+
 template<typename INT>
 void populate_vector(vector<INT>& v, size_t n) {
-    for (size_t i = 0; i < n; ++i) {
+    for (auto i = 0; i < n; ++i) {
         v.emplace_back(i);
     }
+}
+
+template<typename INT>
+vector<INT>&& populate_vector_ref(size_t n) {
+    auto v = new vector<INT>;
+
+    for (auto i = 0; i < n; ++i) {
+        v->emplace_back(i);
+    }
+
+    return move(*v);
 }
 
 template<typename INT>
@@ -20,8 +39,8 @@ void print_vector(vector<INT>& v) {
 }
 
 int main() {
-    vector<int16_t> v;
-    populate_vector(v, 100);
+    auto n = read_number("数字を入力してください > ");
+    auto v = populate_vector_ref<uint32_t>(n);
     print_vector(v);
 
     return 0;
